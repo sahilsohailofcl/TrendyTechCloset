5b82f49725f1d20c75a198fb854a42a38e8e7ae8
sahilsohailofcl <sahilsohailofcl@gmail.com>
2024-08-31 09:20:46 +0500

    add directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './assets/sass/main.sass'
+import './assets/sass/index.sass'
 import './App.css'
 
 function App() {
